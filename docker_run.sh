4ec183f4292ed489a7a6cb121b6b896022676044
Simone D'Angelo <67595147+Simone-DAngelo@users.noreply.github.com>
2026-03-02 10:59:14 +0100

    Add init_sim.sh script to docker run command

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 xhost +local:root
 IMAGE_ID=$(docker images -q $1)
 WORKSPACE_DIR=$(pwd)/src
-docker run --rm -it --name=$2 --net=host -e GZ_PARTITION=shared_world -e GZ_VERBOSE=4  --env="DISPLAY=$DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" --privileged -v /dev:/dev --volume=${WORKSPACE_DIR}/pkg:/home/user/ros2_ws/src/pkg  $1
+docker run --rm -it --name=$2 --net=host -e GZ_PARTITION=shared_world -e GZ_VERBOSE=4  --env="DISPLAY=$DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" --privileged -v /dev:/dev --volume=${WORKSPACE_DIR}/pkg:/home/user/rover_ws/src/pkg -v $(pwd)/init_sim.sh:/home/user/init_sim.sh:rw  $1 /home/user/init_sim.sh
 xhost -local:root
